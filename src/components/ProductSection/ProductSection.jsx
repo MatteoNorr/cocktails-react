dc288c70da63cdd29431821f996e67c06d33d87d
MatteoNorr <matteo.norrito@outlook.com>
2023-06-27 11:32:19 +0200

    feat: product section

---
+++ src/components/ProductSection/ProductSection.jsx
@@ -0,0 +1,13 @@
+const ProductSection = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};

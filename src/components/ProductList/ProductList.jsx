bc585b74dbaea9c118feb4a1bd7286d4bfdb81fb
Matt <MatteoNorr@users.noreply.github.com>
2023-06-30 17:30:57 +0200

    feat: filters

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,24 +2,24 @@ import { useState, useEffect } from "react";
 import ProductItem from "../ProductItem";
 import "./index.css";
 
-const ProductList = ({ name, setSection }) => {
+const ProductList = ({ name, setSection, category }) => {
   const [list, setList] = useState([]);
 
   useEffect(() => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + name)
       .then((res) => res.json())
       .then((data) => setList(data.drinks));
-  }, []);
+  }, [category]);
 
   return (
     <>
       <h1 className="ProductSection__title">{name}</h1>
       <div className="ProductList">
         <div className="ProductList__container">
-          {list.map((data) => (
+          {list.map((data, i) => (
             <div
               className="ProductItem__container"
-              key={data.idDrink}
+              key={i}
               onClick={setSection}
             >
               <ProductItem data={data} />
